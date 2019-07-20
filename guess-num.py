dda9aaa3b7a35e8b5ccb18b2af9414321a6fafe7
KAO HSU YANG <savemoneykey@gmail.com>
2019-07-21 04:28:48 +0800

    final

--- guess-num.py
+++ guess-num.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('請選擇開始範圍: ')
+end = input('請選擇結束範圍: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True :
 	count += 1
